34f36143f0f3063b6d0c6e77d29345a5246e467e
Cristian <cristguevaraolaya@gmail.com>
2023-11-30 14:24:42 -0500

    tennnis

--- src/app/api/auth/check/route.js
+++ src/app/api/auth/check/route.js
@@ -4,17 +4,14 @@ import jwt from "jsonwebtoken";
 import generarJWT from "@/utils/generateJWT";
 import User from "@/models/users";
 import { connectDB } from "@/utils/mongoose";
+
 export async function GET(){
     try{
         connectDB()
 
         const headersList = headers();
-        //const token = headersList.get("token");
-        //console.log(token)
-        return NextResponse.json({
-            ok:false,
-            tokenHeader:headersList
-        })
+        const token = headersList.get("token");
+        console.log(token)
         
         if(!token){
             return NextResponse.json({
@@ -23,6 +20,7 @@ export async function GET(){
         }
 
         const { uid, email, isAdmin } = jwt.verify( token, process.env.SECRET_JWT_SEED );
+
         const newToken = await generarJWT(uid,email,isAdmin)
 
         const userFind = await User.findOne({ email });
